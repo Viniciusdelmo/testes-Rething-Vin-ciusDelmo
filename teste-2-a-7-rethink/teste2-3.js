632b38b6b6feadbd49c0c56ff11a97aaf8d82fc1
Vinícius Delmo <103666432+Viniciusdelmo@users.noreply.github.com>
2022-11-06 18:57:24 -0300

    Update teste2-3.js

--- teste-2-a-7-rethink/teste2-3.js
+++ teste-2-a-7-rethink/teste2-3.js
@@ -1,15 +1,18 @@
 const pessoas = [
-  { name: "Fabiana Araújo", age:33},
-  { name: "Gabriel Gomes", age:25},
-  { name: "Fernando Henrique", age:17},
-  { name: "Ana Luiza", age:2},
-  { name: "Geralda do Nascimento", age:93},
-  { name: "Miguel Souza", age:70},
-  { name: "Antonio Miguel", age:69},
-]
+  { name: "Fabiana Araújo", age: 33 },
+  { name: "Gabriel Gomes", age: 25 },
+  { name: "Fernando Henrique", age: 17 },
+  { name: "Ana Luiza", age: 2 },
+  { name: "Geralda do Nascimento", age: 93 },
+  { name: "Miguel Souza", age: 70 },
+  { name: "Antonio Miguel", age: 69 },
+];
 
-  function localizaCadastro(nome){
-    return nome.name === 'Miguel Souza';
-  }
-  
-  console.log(pessoas.find(localizaCadastro));
+function localizaCadastro(pessoas, nome) {
+  const pessoa = pessoas.find((pessoa) => {
+    return pessoa.name === nome;
+  });
+  return pessoa;
+}
+
+console.log(localizaCadastro(pessoas, "Gabriel Gomes"));
